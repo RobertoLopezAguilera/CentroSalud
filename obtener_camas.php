112f8fba40179a93fa42404862c88c706cf26038
lazar <HeidyLazaro>
2024-07-11 00:15:47 -0600

    modificacion habitacion

--- obtener_camas.php
+++ obtener_camas.php
@@ -2,7 +2,7 @@
 include 'includes/conexion.php';
 
 $id_habitacion = intval($_GET['id_habitacion']);
-$sql_camas = "SELECT id_cama, numero_cama FROM Camas WHERE id_habitacion = ?";
+$sql_camas = "SELECT id_cama, numero_cama FROM Camas WHERE estado = 'DISPONIBLE' and id_habitacion = ?";
 $stmt_camas = $conn->prepare($sql_camas);
 $stmt_camas->bind_param("i", $id_habitacion);
 $stmt_camas->execute();
