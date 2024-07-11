54e281bdf08b57a7de66e736adadd0d22434084a
lazar <HeidyLazaro>
2024-07-11 00:38:14 -0600

    validacion

--- editar_cita.php
+++ editar_cita.php
@@ -6,6 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Cita Medica</title>
     <link rel="stylesheet" href="css/style.css">
+    <script>
+        function validateForm() {
+            var fechaCita = new Date(document.getElementById("fecha_hora").value);
+            var fechaActual = new Date();
+            fechaActual.setHours(0, 0, 0, 0); // Resetear la hora a las 00:00
+
+            var fechaLimite = new Date();
+            fechaLimite.setDate(fechaLimite.getDate() + 2);
+            fechaLimite.setHours(0, 0, 0, 0);
+            if (fechaCita < fechaLimite) {
+                alert("La fecha de la cita debe ser al menos dos días a partir de hoy.");
+                return false;
+            }
+
+            const nombreValue = document.getElementById("tipo").value;
+            const nombrePattern = /^[A-Za-z\u00C0-\u017F\s]+$/;
+
+            if (!nombrePattern.test(nombreValue)) {
+                alert('El tipo de cita solo puede contener letras, espacios y acentos.');
+                event.preventDefault();
+                return;
+            }
+
+            if (nombreValue.length < 5) {
+                alert('El tipo de cita debe tener al menos 5 caracteres.');
+                event.preventDefault();
+            }
+            return true;
+        }
+    </script>
 </head>
 
 <body>
@@ -13,7 +43,7 @@
     <div id="header"></div>
 
     <h1>Editar Cita Medica</h1>
-    
+
     <?php
     include 'includes/conexion.php';
     $id = $_GET['id'];
@@ -28,15 +58,15 @@
     $resultado_paciente = $conn->query($sqlPaciente);
     $filaPaciente = $resultado_paciente->fetch_assoc();
     ?>
-    
-    <form action="procesar_editar_cita.php" method="post">
-        <input type="hidden" name="id" value="<?php echo $fila['id_cita']; ?>">
+
+    <form action="procesar_editar_cita.php" method="post" onsubmit="return validateForm();">
+        <input type= "hidden" name="id" value="<?php echo $fila['id_cita']; ?>">
         <input type="hidden" name="id_paciente" value="<?php echo $fila['id_paciente']; ?>">
 
         <label for="nombre_paciente">Paciente:</label>
-<input type="text" id="nombre_paciente" name="nombre_paciente"
-    value="<?php echo isset($filaPaciente['nombre_paciente']) ? htmlspecialchars($filaPaciente['nombre_paciente']) : ''; ?>"
-    required disabled><br>
+        <input type="text" id="nombre_paciente" name="nombre_paciente"
+            value="<?php echo isset($filaPaciente['nombre_paciente']) ? htmlspecialchars($filaPaciente['nombre_paciente']) : ''; ?>"
+            required disabled><br>
 
         <label for="id_personal">Medico:</label>
         <select id="id_personal" name="id_personal" required>
@@ -51,8 +81,8 @@
         </select><br>
 
         <label for="fecha_hora">Fecha de cita:</label>
-        <input type="datetime-local" id="fecha_hora" name="fecha_hora" value="<?php echo $fila['fecha_hora']; ?>"
-            required><br>
+        <input type="datetime-local" id="fecha_hora" maxlength="100" minlength="2" name="fecha_hora"
+            value="<?php echo $fila['fecha_hora']; ?>" required><br>
 
         <label for="tipo">Tipo:</label>
         <input type="text" id="tipo" name="tipo" value="<?php echo $fila['tipo']; ?>" required><br>
